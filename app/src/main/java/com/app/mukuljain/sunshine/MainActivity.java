cee9589e0a3792d81d1aab58e055be39cf214149
U-MUKUL-PC\mukuljain <jainmukul1985@gmail.com>
2015-10-19 01:08:24 +0100

    adding the list view with dummy data included in an fragment and framelayout

--- app/src/main/java/com/app/mukuljain/sunshine/MainActivity.java
+++ app/src/main/java/com/app/mukuljain/sunshine/MainActivity.java
@@ -1,7 +1,21 @@
 package com.app.mukuljain.sunshine;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +23,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*
+            * A placeholder fragment containing a simple view
+             */
+    public static class PlaceholderFragment extends Fragment {
+
+        ArrayAdapter<String> mForecastAdapter;
+
+        public PlaceholderFragment() {
+
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String[] forecast = {
+                    "Today - Sunny - 88/63",
+                    "Today - Sunny - 88/63",
+                    "Today - Sunny - 88/63",
+                    "Today - Sunny - 88/63",
+                    "Today - Sunny - 88/63",
+                    "Today - Sunny - 88/63",
+                    "Today - Sunny - 88/63",
+                    "Today - Sunny - 88/63",
+                    "Today - Sunny - 88/63",
+                    "Today - Sunny - 88/63",
+                    "Today - Sunny - 88/63"
+            };
+
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecast));
+
+             mForecastAdapter = new ArrayAdapter<String>(getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weekForecast);
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
+
+            return rootView;
+        }
     }
 }
